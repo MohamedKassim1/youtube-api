f8d8cda9ae3a5487cd3cf12ea558a4b88c89ebd3
MohanedKassim <aliyyi89@gmail.com>
2020-07-19 18:42:36 -0500

    search component created

--- src/API/youtube.js
+++ src/API/youtube.js
@@ -1,11 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: '',
-    params:{
-        part: 'snippet',
-        maxResults: 5,
-        key: process.env.REACT_APP_API_KEY,
-        
-    }
+    baseURL: 'https://www.googleapis.com/youtube/v3',
+   
 })
